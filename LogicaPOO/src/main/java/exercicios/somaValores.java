f9fce7d0d9ffa58fd5ef120eac2606fad346e825
Benhur <benhurbbs@gmail.com>
2024-05-01 18:54:36 -0300

    Soma de valores

---
+++ LogicaPOO/src/main/java/exercicios/somaValores.java
@@ -0,0 +1,17 @@
+package exercicios;
+
+public class somaValores {
+    public static void main(String[] args) {
+        double resultado = 0;
+        int soma = 0;
+        for (double x = 1; x <= 100; x++) {
+            resultado += ((1 / 2) + (1 / 3)) / ((x / 4) + (1 / x));
+            System.out.print("x -> " + String.valueOf(x) + " = ");
+            System.out.println(resultado);
+            if (resultado % 1 == 0) {
+                soma += resultado;
+            }
+        }
+        System.out.println("A soma dos valores inteiros da expressão para x de 1 a 100 é: " + soma);
+    }
+}
